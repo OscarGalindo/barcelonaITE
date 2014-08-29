2155010eb605089c07605bab9f53ecbe416c39e1
kaseOga <galindero@gmail.com>
2014-08-29 16:13:42 +0000

    scroll2 and scroll3 done v1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+  $(".arrow").on('click', function() {
+    $("#main").moveDown();
+  });
+  $('#botton_contacta').on('click', function() {
+    $("#main").moveTo(2);
+  });
 
+  $("#main").onepage_scroll({
+     sectionContainer: "section",
+     easing: "ease",
+     animationTime: 1000,
+     pagination: true,
+     updateURL: true,
+     loop: false,
+     keyboard: true,
+     responsiveFallback: false,
+     direction: "vertical"
+  });
+});
